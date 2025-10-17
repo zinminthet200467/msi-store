c9808bcaf75d81dfb81f7aecdc385ceb33a7e3e6
zinminthet200467 <zinminthet200476@gmail.com>
2025-10-17 22:57:34 +0630

    chores(assets): add dummy images

---
+++ assets/images/index.js
@@ -0,0 +1,8 @@
+const appStore = require("./appStore.png");
+const gamePoster = require("./gamePoster.jpg");
+const valorant = require("./valorant.jpg");
+const appLogo = require("./appLogo.png");
+
+const images = { appStore, gamePoster, valorant, appLogo };
+
+export default images;
